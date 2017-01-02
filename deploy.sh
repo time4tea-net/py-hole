a2712c649e6b1094f3b5034882c60af2a796503e
James Richardson <james@time4tea.net>
2017-01-02 01:03:12 +0000

    automatically publish it for now

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 set -u
 set -e
 
-curl -T target/distributions/py-hole_${VERSION}_any.deb -utime4tea:${BINTRAY_API_KEY} https://api.bintray.com/content/time4tea/py-hole/py-hole/${VERSION}/pool/main/p/py-hole/py-hole_${VERSION}_any.deb\;deb_distribution=time4tea\;deb_component=free\;deb_architecture=any
+curl -T target/distributions/py-hole_${VERSION}_any.deb -utime4tea:${BINTRAY_API_KEY} https://api.bintray.com/content/time4tea/py-hole/py-hole/${VERSION}/pool/main/p/py-hole/py-hole_${VERSION}_any.deb\;deb_distribution=time4tea\;deb_component=free\;deb_architecture=any\;publish=1
