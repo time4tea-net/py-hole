39f4e9cdcdd8e9ce3f048146bd0776acc724b482
James Richardson <james@time4tea.net>
2021-10-25 10:02:51 +0100

    iterate on actions...

--- sign.sh
+++ sign.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-echo version ${VERSION:?not set}
 
 KEYID=7EC6BC48A8F76477
 
-echo "Signing with $KEYID"
-
 . ./set-version.sh
+
+echo version ${VERSION:?not set}
+
+echo "Signing with $KEYID"
 
 debsigs --sign=origin -k $KEYID target/distributions/py-hole_${VERSION}_all.deb
 
